f7c80884bac9279f75b431221f18d586aa614ee9
Crowdbotics <engineering@crowdbotics.com>
2024-01-16 07:12:39 +0000

    Update store.

---
+++ store/ghjjyusfbuAPI/api.js
@@ -0,0 +1,69 @@
+import axios from "axios"
+const ghjjyusfbuAPI = axios.create({
+  baseURL: "https://ghjjyusfbu-74.botics.co",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_docs_schema_retrieve(payload) {
+  return ghjjyusfbuAPI.get(`/api-docs/schema/`, {
+    params: { lang: payload.lang }
+  })
+}
+function api_v1_connectors_trimpf001_search_testing_app_retrieve(payload) {
+  return ghjjyusfbuAPI.get(`/api/v1/connectors/trimpf001/search/testing/app/`)
+}
+function api_v1_login_create(payload) {
+  return ghjjyusfbuAPI.post(`/api/v1/login/`, payload)
+}
+function api_v1_signup_create(payload) {
+  return ghjjyusfbuAPI.post(`/api/v1/signup/`, payload)
+}
+function rest_auth_login_create(payload) {
+  return ghjjyusfbuAPI.post(`/rest-auth/login/`, payload)
+}
+function rest_auth_logout_create(payload) {
+  return ghjjyusfbuAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(payload) {
+  return ghjjyusfbuAPI.post(`/rest-auth/password/change/`, payload)
+}
+function rest_auth_password_reset_create(payload) {
+  return ghjjyusfbuAPI.post(`/rest-auth/password/reset/`, payload)
+}
+function rest_auth_password_reset_confirm_create(payload) {
+  return ghjjyusfbuAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+}
+function rest_auth_registration_create(payload) {
+  return ghjjyusfbuAPI.post(`/rest-auth/registration/`, payload)
+}
+function rest_auth_registration_resend_email_create(payload) {
+  return ghjjyusfbuAPI.post(`/rest-auth/registration/resend-email/`, payload)
+}
+function rest_auth_registration_verify_email_create(payload) {
+  return ghjjyusfbuAPI.post(`/rest-auth/registration/verify-email/`, payload)
+}
+function rest_auth_user_retrieve(payload) {
+  return ghjjyusfbuAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return ghjjyusfbuAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return ghjjyusfbuAPI.patch(`/rest-auth/user/`, payload)
+}
+export const apiService = {
+  api_docs_schema_retrieve,
+  api_v1_connectors_trimpf001_search_testing_app_retrieve,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_resend_email_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
